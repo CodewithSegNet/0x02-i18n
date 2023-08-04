fa0f458db5cf05c5db79751b033438e5d7f45546
segunUb18 <eeomo@yahoo.com>
2023-08-04 18:23:22 +0100

    :white_check_mark: Fix app.py to follow pycodestyle
    implement pycodestyle

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -4,11 +4,14 @@ from flask import Flask, render_template
 from flask_babel import Babel
 
 """ config class """
+
+
 class Config:
     """ Represents a Flask babel configuration. """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -21,6 +24,7 @@ def get_index() -> str:
     """The home/index page.
     """
     return render_template('1-index.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
