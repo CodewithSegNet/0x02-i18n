f0956d07e8ec85d8a3ddaddfb786771ffbf62a40
segunUb18 <eeomo@yahoo.com>
2023-08-04 21:45:01 +0100

    :bug: Fix bug
    fix bug in 2-app.pu

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
-""" get_locale function use to determine the best match """
-from flask import Flask, render_template
+"""A Basic Flask app.
+"""
 from flask_babel import Babel
+from flask import Flask, render_template, request
 
 
 class Config:
-    """ represents a flask babel configuration """
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    """Represents a Flask Babel configuration.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
@@ -17,17 +19,19 @@ app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
+@babel.localeselector
+def get_locale() -> str:
+    """Retrieves the locale for a web page.
+    """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
 @app.route('/')
-def index():
-    """ the home/index page.
+def get_index() -> str:
+    """The home/index page.
     """
     return render_template('2-index.html')
 
 
-@babel.localeselector
-""" decorator for get locale """
-def get_locale() -> str:
-    return request.accept_languages.best_match(app.config['LANGUAGE'])
-
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
