0ffed2ed1269cf5bb5697003b929698420474964
segunUb18 <eeomo@yahoo.com>
2023-08-04 19:19:38 +0100

    :white_check_mark: Fix code for 2-app.py
    fix 2-app.py

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -4,20 +4,24 @@
 from flask import Flask, render_template
 from flask_babel import Babel
 
+
 class Config:
     """ represents a flask babel configuration """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
 babel = Babel(app)
 
+
 @app.route('/')
 def index():
     return render_template('2-index.html')
+
 
 @babel.localeselector
 def get_locale() -> str:
